d69b4584d0dd9fca5647102ec8163d7946ae7ce2
Susan Woods <swoods63@msn.com>
2017-08-05 03:05:12 -0600

    Everything seems to be working.

--- itunes-controller.js
+++ itunes-controller.js
@@ -7,26 +7,40 @@ function ItunesController() {
         itunesService.getMusicByArtist(artist).then(drawSong); //after get music by artist returns what are you doing with the objects?
     };
 
+
+
     function drawSong(songList) {
-        var template = ``;
+        var template = ``
         console.log(songList);
         var songs = document.getElementById(songList);
         for (var i = 0; i < 50; i++) {
             var song = songList[i];
             template += `
           <div class="row ">   
+            <div class="col-sm-1 "><img src="${song.albumArt} " /></div>
             <div class="col-sm-2 "><p>Title: ${song.title}</p></div>
-            <div class="col-sm-2 "><img src="${song.albumArt} " /></div>
             <div class="col-sm-2 "><p>Artist: ${song.artist}</p></div>
             <div class="col-sm-2 "><p>Collection: ${song.collection}</p></div>
             <div class="col-sm-2 "><p>Price: ${song.price}</p></div>
-            <div class="col-sm-2 "><audio controls>
+            <div class="col-sm-3 "><audio controls>
             <source src="${songList[i].preview} " type="audio/ogg "></audio></div>
-          </div>
+          </div> `
 
-            `;
-            song.innerHTML = template
         }
+
+        song.innerHTML = template;
+
         document.getElementById("song-list").innerHTML = template;
+
+        $(function() {
+            $("audio").on("play", function() {
+                $("audio").not(this).each(function(index, audio) {
+                    audio.pause();
+                });
+            });
+        });
+
     }
 }
+
+var itunesCtrl = new ItunesController()
