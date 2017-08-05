c73943e786bd92befe822e3889931f3c4135bf07
Susan Woods <swoods63@msn.com>
2017-08-04 21:08:41 -0600

    Got music on and page and can play song.

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,17 +1,32 @@
-function ItunesController(){
-  var itunesService = new ItunesService()
-  //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
-    e.preventDefault();
-    var artist = e.target.artist.value;
-    itunesService.getMusicByArtist(artist).then(); //after get music by artist returns what are you doing with the objects?
-  }
+function ItunesController() {
+    var itunesService = new ItunesService()
+        //Do Not Modify the getMusic function
+    this.getMusic = function getMusic(e) {
+        e.preventDefault();
+        var artist = e.target.artist.value;
+        itunesService.getMusicByArtist(artist).then(drawSong); //after get music by artist returns what are you doing with the objects?
+    };
 
-  //Start coding here
+    function drawSong(songList) {
+        var template = ``;
+        console.log(songList);
+        var songs = document.getElementById(songList);
+        for (var i = 0; i < 50; i++) {
+            var song = songList[i];
+            template += `
+          <div class="row ">   
+            <div class="col-sm-2 "><p>Title: ${song.title}</p></div>
+            <div class="col-sm-2 "><img src="${song.albumArt} " /></div>
+            <div class="col-sm-2 "><p>Artist: ${song.artist}</p></div>
+            <div class="col-sm-2 "><p>Collection: ${song.collection}</p></div>
+            <div class="col-sm-2 "><p>Price: ${song.price}</p></div>
+            <div class="col-sm-2 "><audio controls>
+            <source src="${songList[i].preview} " type="audio/ogg "></audio></div>
+          </div>
 
-
-
-
-
-  
+            `;
+            song.innerHTML = template
+        }
+        document.getElementById("song-list").innerHTML = template;
+    }
 }
